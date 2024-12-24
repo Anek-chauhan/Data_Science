15dcaa029cbaec11547bff69b4d26fa00cc186bb
anekchauhan26@gmail.com <Anek@9219>
2024-12-24 19:02:21 +0530

    Final update to fibonachi

--- Fibonachi.py
+++ Fibonachi.py
@@ -1,8 +1,8 @@
-dict1=[0,1]
 def fib(nth_value):
-    if(nth_value== 0):
+    dict1=[0,1]
+    if(nth_value== 1):
         print([0])
-    else:
+    if(nth_value>1):
         for i in range(1,nth_value-1):
             dict1.append(list(dict1)[-1]+list(dict1)[-2])
         print(dict1)    
