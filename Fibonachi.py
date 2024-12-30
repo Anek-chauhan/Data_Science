db2544994725bf828b184fde62f0d3eaf78ab7d8
anekchauhan26@gmail.com <Anek@9219>
2024-12-30 18:56:25 +0530

    Assignmet added

--- Fibonachi.py
+++ Fibonachi.py
@@ -1,10 +1,10 @@
-def fib(nth_value):
+def fibonacci(n):
     dict1=[0,1]
-    if(nth_value== 1):
+    if(n== 1):
         print([0])
-    if(nth_value>1):
-        for i in range(1,nth_value-1):
+    if(n>1):
+        for i in range(1,n):
             dict1.append(list(dict1)[-1]+list(dict1)[-2])
-        print(dict1)    
-nth_value=int(input("Enter nth value "))
-fib(nth_value)          
+        return  dict1
+n = int(input())
+print(fibonacci(n))    
